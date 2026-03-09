43d7df6bd169d81ae0a2ff875917fe9ca9878ece
ibrahimkodehode-png <ibrahim.kodehode@gmail.com>
2026-03-09 11:29:10 +0100

    menu jsxxx

---
+++ src/components/Menu.jsx
@@ -0,0 +1,20 @@
+import Dish from "./Dish";
+import styles from "./Menu.module.css";
+
+function Menu({ dishes }) {
+  return (
+    <section className={styles.menuGrid}>
+      {dishes.map((dish) => (
+        <Dish
+          key={dish.id}
+          tittel={dish.tittel}
+          pris={dish.pris}
+          ingredienser={dish.ingredienser}
+          kategori={dish.kategori}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default Menu;
